eb152329751e8c90eddc261eac8610eb57a1847c
Zuhair Ali <szali172@gmail.com>
2022-05-25 17:00:48 -0500

    Simple image retrieval working

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify, send_file
 from datetime import datetime
 from PIL import Image
-from bson import Binary, ObjectId
+from bson.objectid import ObjectId
 from bson.json_util import dumps
-import os, io, requests
+import os, io, requests, sys
 
 import config as c
 import hash
@@ -14,20 +14,31 @@ import pymongo
 cluster = pymongo.MongoClient(f"mongodb+srv://{c.username}:{c.pwd}@pilots.nvl9y.mongodb.net/Pilots?retryWrites=true&w=majority")
 
 
+
+
 # route for "/" (frontend):
 @app.route('/')
 def index():
   return render_template("index.html")
+
+
+
 
 # Route for the archives (frontend)
 @app.route('/gallery')
 def GET_gallery():
     return render_template("gallery.html")
 
+
+
+
 # Route for returning a specific image
 @app.route('/images/image-<x>.jpg', methods=['GET'])
 def GET_image(x):
     return send_file(f'images/image-{x}.jpg')
+
+
+
 
 # Route for returning the space background video
 @app.route('/space.mp4', methods=['GET'])
@@ -35,19 +46,23 @@ def GET_video():
     return send_file(f'templates/space.mp4')
   
 
+
+
 # Routes to retrieve, insert or delete a specified file
 @app.route('/<db>/<coll>/<filename>', methods=['PUT', 'GET', 'DELETE'])
 def PUT_GET_DEL_image(db, coll, filename):
     
     # Check to see if db and collection exist
     try:
-        db = cluster[db] # archives
-        collection = db[coll] # tuscola, 404, etc.
+        database = cluster[db] # archives
+        collection = database[coll] # tuscola, 404, etc.
     except:
         return f'Could not access {db} or {coll}\n', 400
+    
         
     # Route to add a file into the specified db
     if request.method == 'PUT':
+        
         try:
             # check if filename already exists
             cursor = collection.find_one({'filename': filename})
@@ -59,59 +74,96 @@ def PUT_GET_DEL_image(db, coll, filename):
             im.save(image_bytes, format='JPEG')
             
             image_b = {'data': image_bytes.getvalue()}
-            collection.insert_one({'filename': filename, 'image': image_b, 'UTC': datetime.now()}) 
-            return jsonify({'image': image_b}), 200
+            collection.insert_one({'filename': str(filename), 'image': image_b, 'UTC': datetime.now()}) 
+            return 'Success', 200
         except:
             return f'{filename} could not be added\n', 400
         
+        
     # Route to get a file from the specified db and collection
     if request.method == 'GET':
+        
         try:
             cursor = collection.find_one({'filename': filename})
             image_b = cursor['image']['data']
             image = Image.open(io.BytesIO(image_b))
-            image.save("image.jpg")
+            image.save(f'{db}/{coll}/{filename}')  
         except:
-            return f'Could not find {filename}\n', 400       
-        send_file(image)
-        return f'{filename} found', 200
+            return f'Could not find {filename}\n', 400    
+           
+        return send_file(f'{db}/{coll}/{filename}'), 200
+        # return f'{filename} found\n', 200
+    
     
     # Route to delete a file from the specific db and collection
     if request.method == 'DELETE':
+        
         try:
-            collection.delete_one({'filename': filename})
+            collection.delete_one({'filename': str(filename)})
             return f'Successfully deleted {filename} from {coll}\n', 200
         except:
             return 'File could not be deleted\n', 400
+ 
+ 
         
         
 # Route to retrieve or delete all files in a specific collection
-@app.route('/<db>/<coll>', methods=['GET', 'DELETE'])
-def GET_images(db, coll):
+# 'CLEAR' endpoint deletes all local files in the specified db/coll directory
+@app.route('/<db>/<coll>', methods=['GET', 'DELETE', 'CLEAR'])
+def GET_DEL_CLEAR_images(db, coll):
+    
+    # Check to see if db and collection exist
+    try:
+        database = cluster[db] # archives
+        collection = database[coll] # tuscola, 404, etc.
+    except:
+        return f'Could not access {db} or {coll}\n', 400
+   
     
     if request.method == 'GET':
-        try:
-            db = cluster[db] # archives
-            collection = db[coll] # tuscola, 404, etc.
-        except:
-            return 'Could not access db or collection\n', 400
         
         cursor = collection.find({})
-        images = []
-        for item in cursor:
-            with open(item['image'], 'rb') as image:         
-                images.append(image)
+        filenames = []
         
-        return jsonify({'images': images}), 200
+        for item in cursor:
+            
+            f_name = str(item['filename']).strip()
+            filenames.append(f_name)
+            image = Image.open(io.BytesIO(item['image']['data']))
+            image.save(f'{db}/{coll}/{f_name}')
+        
+        return jsonify({"filenames": filenames}), 200
+   
     
     if request.method == 'DELETE':
+        
         try:
             collection.delete_many({})
             return f'Successfully deleted all files in {coll}\n', 200
+        
         except:
-            return f'Files in {coll} could not be deleted\n', 400    
+            return f'Files in {coll} could not be deleted\n', 400
+        
+        
+    if request.method == 'CLEAR':
+        
+        dir = f'{db}/{coll}'
+        try:
+            
+            for filename in os.listdir(dir):
+            
+                f = os.path.join(dir, filename)
+                os.remove(f)
+            
+            return f'Cleared {db}/{coll} directory\n', 200
+        
+        except:
+            return f'Could not clear {db}/{coll} directory\n', 400   
+
+
+        
+        
 
 # TODO: fix video loop
-# TODO: add button for archives page on index.html
 # TODO: fix delete_one endpoint
-# TODO: Figure out unique filenames when inserting them into mongo
+# TODO: connect js with app.py
--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,26 +1,75 @@
+import { getFilenames } from "./filenames.js";
+
 const gallery = document.getElementById('gallery');
 const popup = document.getElementById('popup');
 const selectedImage = document.getElementById('selectedImage');
 const imageIndexes = [1, 2, 3, 4];
 const selectedIndex = null;
 
-imageIndexes.forEach(i => {
-    const image = document.createElement('img');
-    image.src = `../../images/image-${i}.jpg`
-    image.alt = `Cover for episode ${i} of the compressed blah blah`
-    image.classList.add('galleryImg');
+// var arr = [];
 
-    image.addEventListener('click', () => {
-        //popup stuff
-        popup.style.transform = 'translateY(0)';
-        selectedImage.src = `../../images/image-${i}.jpg`
-        selectedImage.alt = `Cover for episode ${i} of the compressed blah blah`
+// let f_names = await getFilenames("archives", "tuscola");
+// console.log("filenames: " + f_names.filenames);
+// let f_names = fetch(`http://127.0.0.1:5000/archives/tuscola`)
+// .then((response) => {
+//     if (!response.ok) {
+//         throw new Error("HTTP error " + response.status);
+//     }
+//     // f_names = response.json();
+//     // console.log(f_names);
+//     arr = response.json().filenames;
+// });
+
+$.getJSON('http://127.0.0.1:5000/archives/tuscola', function(data) {
+
+    // Get array of filenames created in directory
+    const f_names = data.filenames;
+    console.log(f_names);
+
+    f_names.forEach(i => {
+        const image = document.createElement('img');
+        image.src = `../../archives/tuscola/${i}`
+        image.alt = `Alternate image goes here ${i}`
+        image.classList.add('galleryImg');
+
+        image.addEventListener('click', () => {
+            //popup stuff
+            popup.style.transform = 'translateY(0)';
+            selectedImage.src = `../../archives/tuscola/${i}.jpg`
+            selectedImage.alt = `Cover for episode ${i} of the compressed blah blah`
+        })
+        gallery.appendChild(image);
     })
-    gallery.appendChild(image);
-})
+    
+    popup.addEventListener('click', () => {
+        popup.style.transform = 'translateY(-100%)';
+        popup.src = '';
+        popup.alt = '';
+    })
+    
+});
 
-popup.addEventListener('click', () => {
-    popup.style.transform = 'translateY(-100%)';
-    popup.src = '';
-    popup.alt = '';
-})
+
+// console.log("f_names: " + f_names);
+
+
+// imageIndexes.forEach(i => {
+//     const image = document.createElement('img');
+//     image.src = `../../images/image-${i}.jpg`
+//     image.alt = `Alternate image goes here ${i}`
+//     image.classList.add('galleryImg');
+
+//     image.addEventListener('click', () => {
+//         //popup stuff
+//         popup.style.transform = 'translateY(0)';
+//         selectedImage.src = `../../images/image-${i}.jpg`
+//         selectedImage.alt = `Cover for episode ${i} of the compressed blah blah`
+//     })
+//     gallery.appendChild(image);
+// })
+
+// popup.addEventListener('click', () => {
+//     popup.style.transform = 'translateY(-100%)';
+//     popup.src = '';
+//     popup.alt = '';
+// })
